6666a45b0530d4af9ae8d522a84696a621388b4f
stefanie-cyber <stefaniejvv80@gmail.com>
2026-05-27 12:04:13 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Section_B_Greeting_message
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnGreeting = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(175, 156);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(130, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Enter your name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(329, 156);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(135, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnGreeting
+            // 
+            this.btnGreeting.Location = new System.Drawing.Point(358, 257);
+            this.btnGreeting.Name = "btnGreeting";
+            this.btnGreeting.Size = new System.Drawing.Size(97, 45);
+            this.btnGreeting.TabIndex = 2;
+            this.btnGreeting.Text = "Greeting";
+            this.btnGreeting.UseVisualStyleBackColor = true;
+            this.btnGreeting.Click += new System.EventHandler(this.btnGreeting_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGreeting);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnGreeting;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Section_B_Greeting_message
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGreeting_Click(object sender, EventArgs e)
+        {
+            //txtname in string ,user enter 
+            string name = txtName.Text;
+              
+            //desplay message
+            MessageBox.Show("Hello " + name + "!");
+
+        }
+    }
+}
